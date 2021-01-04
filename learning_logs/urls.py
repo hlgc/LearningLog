d149a2784d3255dd42a944d61238c7f639792eb5
Kevin <hao1iu@qq.com>
2021-01-04 23:48:40 +0800

    bug fix

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
     # 用于添加新条目的页面
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # 用于编辑条目的页面
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
